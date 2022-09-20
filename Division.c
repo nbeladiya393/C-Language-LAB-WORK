4ba0782ebd4d9a2f25ce44c5bdc60d92b48ba59b
nbeladiya393 <nbeladiya393@gmail.com>
2022-09-20 19:08:12 +0530

    first commit

---
+++ Division.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+main(){
+     int a=10;
+	 int b=5;
+	 int c;
+	 
+	 c = a/b;
+	 
+	 printf("%d",c);	
+
+	
+}
---
+++ subtraction.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+main(){
+     int a=10;
+	 int b=20;
+	 int c;
+	 
+	 c = a-b;
+	 
+	 printf("%d",c);	
+
+	
+}
